1ee15dcd332e7d64519a4aad304a26f0d21aae66
Manfred Steyer <manfred.steyer@gmx.net>
2020-05-09 14:43:51 +0200

    initial commit

---
+++ apps/flight-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'flights-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'flight-app';
+}
